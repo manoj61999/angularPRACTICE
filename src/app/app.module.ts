beddfc1b3c0f94b17213380a7277863bfd0b71af
MANOJKUMAR R <89155515+manoj61999@users.noreply.github.com>
2021-08-22 21:07:06 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';                 //Reactive form method
 import { PracticeComponent } from './practice/practice.component';
 import { SampleComponent } from './sample/sample.component';
 import { ExcerComponent } from './excer/excer.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { HttpClient,HttpClientModule } from '@angular/common/http';   //HTTP Service
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { CustomerComponent } from './customer/customer.component';
 @NgModule({
@@ -26,11 +26,11 @@ import { CustomerComponent } from './customer/customer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    FormsModule,               
+    ReactiveFormsModule,      //formGroup formControl
+    HttpClientModule         //HTTP Service
   ],
-  providers: [HttpClient],
+  providers: [HttpClient],    //HTTP Service
   bootstrap: [AppComponent]
 })
 export class AppModule { }
