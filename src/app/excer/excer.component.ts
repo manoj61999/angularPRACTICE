3601429ad6a159710e33fcb1179b1d9fa48a6bcd
MANOJKUMAR R <89155515+manoj61999@users.noreply.github.com>
2021-08-22 20:51:54 +0530

    Update excer.component.ts

--- src/app/excer/excer.component.ts
+++ src/app/excer/excer.component.ts
@@ -1,3 +1,5 @@
+//pipes method
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,25 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcerComponent implements OnInit {
 
- 
+ //year and time
   
   currentDate = Date.now();
   numDateFormat = 1478496544151;
   getYear = 'Tue Dec 12 2018 11:20:18 GMT+0530';
   getTime = 'Wed Jan 20 2019 12:20:18 GMT+0530';
+  
+  //currency 
 
   itemPrice1: number = 5.50;
   itemPrice2: number = 10;
   itemPrice3: number = 50;
+  
+  //Decimal value eg:7.436
 
   myNum1: number = 7.4364646;
   myNum2: number = 0.92343456;
+  
+  //Percentage
 
   numA: number = 0.349;
   numB: number = 2.4595;
+  
+  //LowerCase & UpperCase
 
-  convertText1: string = "I AM BEING MANAGED BY PIPES";
-  convertText2: string = "I Am Being Managed By Pipes";
+  convertText1: string = "I AM BEING MANAGED BY PIPES";  //LowerCase
+  convertText2: string = "I Am Being Managed By Pipes";  //UpperCase
+  
+  //Slice 
 
   Users= ["name","age","class","degree","submit","success"]
   // Users= [1,2,3,4,5,6,7,8,9,0,3,4,5]
